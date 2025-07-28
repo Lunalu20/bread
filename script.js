ffe0a3ad00db62efb9587996034cf9771fc5adca
Melvinan <melvinan@student.21-school.ru>
2025-07-28 21:04:12 +0600

    df

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-<script src="script.js"></script>
+function choose(option) {
+    const panel = document.getElementById("result-panel");
+    const result = document.getElementById("result-text");
+  
+    if (option === "csharp") {
+      result.innerText = "Леонид: C# — это мой фетиш. Но с твоим pull request, Ксюша, даже компилятор покраснел...";
+    } else {
+      result.innerText = "Леонид: Секс — под вопросом, а вот мой код уже готов к деплою. И да, у меня не только код красивый — посмотри на мой ASSignment operator!";
+    }
+  
+    panel.hidden = false;
+  }
+  
